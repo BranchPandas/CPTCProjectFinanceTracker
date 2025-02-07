a6e72f810fe3ae7de0600481290ab73082e00372
William Owen <william.owen.bsc@gmail.com>
2025-02-07 13:04:24 -0800

    Set up for Budget Screen

---
+++ CPTCProjectFinanceTracker/BudgetScreen.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CPTCProjectFinanceTracker
+{
+    partial class BudgetScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            button1 = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 59);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Size = new Size(1693, 848);
+            splitContainer1.SplitterDistance = 563;
+            splitContainer1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 4;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(255, 192, 192);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            button1.Location = new Point(138, 773);
+            button1.Name = "button1";
+            button1.Size = new Size(279, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Add Budget Item";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label1.Location = new Point(30, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(523, 72);
+            label1.TabIndex = 1;
+            label1.Text = "Create Budget Item";
+            // 
+            // BudgetScreen
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1717, 919);
+            Controls.Add(splitContainer1);
+            Name = "BudgetScreen";
+            Text = "Budget Screen";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button button1;
+        private Label label1;
+    }
+}
