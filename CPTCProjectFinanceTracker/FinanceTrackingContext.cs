eb4fe10277e86198c859787d8ada331af6fd624c
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-02-11 13:23:53 -0800

    Refactor FinanceTrackingContext and replace Income with Transactions
    Refactored FinanceTrackingContext for better structure and readability.
    Replaced DbSet<Income> with DbSet<Transactions> to shift focus from
    tracking incomes to tracking transactions. Removed the Income class
    and added a new Transactions class with properties for transaction
    details. Created a new migration to update the database schema,
    dropping the Incomes table and creating a new Transactions table.
    Updated the model snapshot to reflect these changes.

--- CPTCProjectFinanceTracker/FinanceTrackingContext.cs
+++ CPTCProjectFinanceTracker/FinanceTrackingContext.cs
@@ -5,29 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CPTCProjectFinanceTracker
+namespace CPTCProjectFinanceTracker;
+
+public class FinanceTrackingContext : DbContext
 {
-    public class FinanceTrackingContext : DbContext
-    {
-        public FinanceTrackingContext() 
-        { 
-        
-        }
-        /// <summary>
-        /// This is how the database is being created and its connection string
-        /// </summary>
-        /// <param name="options"></param>
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            // Database = The desired name for the database
-            // Server = The server we are connecting to. LocalDB is included with Visual Studio
-            // Trusted_Connection = True means we are using Windows Authentication
-            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CPTCFinanceTracker;Trusted_Connection=True;");
-        }
-        /// <summary>
-        /// This tells EF Core to track Incomes in the database.
-        /// This is the table that will be created in the database.
-        /// </summary>
-        public DbSet<Income> Incomes { get; set; }
+    public FinanceTrackingContext() 
+    { 
+    
     }
+    /// <summary>
+    /// This is how the database is being created and its connection string
+    /// </summary>
+    /// <param name="options"></param>
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Database = The desired name for the database
+        // Server = The server we are connecting to. LocalDB is included with Visual Studio
+        // Trusted_Connection = True means we are using Windows Authentication
+        options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CPTCFinanceTracker;Trusted_Connection=True;");
+    }
+    /// <summary>
+    /// This tells EF Core to track Transactions in the database.
+    /// This is the table that will be created in the database.
+    /// </summary>
+    public DbSet<Transactions> Transactions { get; set; }
 }
