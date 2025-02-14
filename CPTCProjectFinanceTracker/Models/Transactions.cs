3b42d053ebfeb461bccbf3764885f15ca3d497bd
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-02-14 09:52:46 -0800

    - Refactor project structure to Model the MVC pattern
    Updated project organization by moving source files to the Views directory.
    Added TransactionController and UserController for transaction and user management. Enhanced FinanceTrackingContext with new DbSet properties for Accounts, Users, and Reminders.
    Updated migration files to reflect database schema changes, including renaming Incomes to Transactions.
    Closes #60

---
+++ CPTCProjectFinanceTracker/Models/Transactions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPTCProjectFinanceTracker.Models;
+
+public class Transactions
+{
+    /// <summary>
+    /// Unique identifier for each transaction
+    /// </summary>
+    [Key]
+    public int TransactionId { get; set; }
+    /// <summary>
+    /// The date of the transaction was created
+    /// </summary>
+    public DateOnly TransactionDate { get; set; }
+    /// <summary>
+    /// The account that the transaction is associated with
+    /// </summary>
+    [Required]
+    public int AccountId { get; set; }
+    /// <summary>
+    /// The amount of the transaction
+    /// </summary>
+    [Required]
+    public decimal TransactionAmount { get; set; }
+    /// <summary>
+    /// The type of the transaction (Income or Expense)
+    /// </summary>
+    [Required]
+    public string? TransactionType { get; set; }
+    /// <summary>
+    /// A short description of the transaction (optional)
+    /// </summary>
+    public string? TransactionDescription { get; set; }
+    /// <summary>
+    /// The category of the transaction
+    /// </summary>
+    [Required]
+    public int CategoryId { get; set; }
+
+}
