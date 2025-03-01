f1ca751d6b048980bc52207c3cfbf11ebce94197
William Owen <william.owen.bsc@gmail.com>
2025-03-01 12:23:14 -0800

    Placeholder bar graph

--- CPTCProjectFinanceTracker/Views/Graphs/Graph1Form.Designer.cs
+++ CPTCProjectFinanceTracker/Views/Graphs/Graph1Form.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Graph1Form";
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            sqlDataAdapter1 = new Microsoft.Data.SqlClient.SqlDataAdapter();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(12, 12);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(1703, 701);
+            chart1.TabIndex = 0;
+            chart1.Text = "chart1";
+            // 
+            // Graph1Form
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1739, 754);
+            Controls.Add(chart1);
+            Name = "Graph1Form";
+            Text = "Graph1Form";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Microsoft.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
