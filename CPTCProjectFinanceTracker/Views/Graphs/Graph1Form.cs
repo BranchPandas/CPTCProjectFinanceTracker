73ba9f308a9be7edfb5969c9661f099c9d812886
William Owen <william.owen.bsc@gmail.com>
2025-03-01 11:13:10 -0800

    Created a dropdown that has links to two new forms that can contain different types of charts/graphs/data viz.

---
+++ CPTCProjectFinanceTracker/Views/Graphs/Graph1Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPTCProjectFinanceTracker.Views.Graphs
+{
+    public partial class Graph1Form : Form
+    {
+        public Graph1Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CPTCProjectFinanceTracker/Views/HomeScreen.Designer.cs
+++ CPTCProjectFinanceTracker/Views/HomeScreen.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeScreen));
             label1 = new Label();
             btnAddIncome = new Button();
             btnAddExpense = new Button();
@@ -35,15 +36,20 @@
             grpBxTransactions = new GroupBox();
             dgvRecentTransactions = new DataGridView();
             label2 = new Label();
+            toolStrip2 = new ToolStrip();
+            toolStripDropdown_Graphs = new ToolStripDropDownButton();
+            toolStripMenuItem_graph1 = new ToolStripMenuItem();
+            ToolStripMenuItem_Graph2 = new ToolStripMenuItem();
             grpBxTransactions.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvRecentTransactions).BeginInit();
+            toolStrip2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(29, 135);
+            label1.Location = new Point(29, 195);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
             label1.Size = new Size(292, 46);
@@ -81,7 +87,7 @@
             // txtCurrentBalance
             // 
             txtCurrentBalance.Enabled = false;
-            txtCurrentBalance.Location = new Point(360, 135);
+            txtCurrentBalance.Location = new Point(360, 195);
             txtCurrentBalance.Margin = new Padding(4, 3, 4, 3);
             txtCurrentBalance.Name = "txtCurrentBalance";
             txtCurrentBalance.Size = new Size(860, 52);
@@ -92,11 +98,11 @@
             grpBxTransactions.Controls.Add(dgvRecentTransactions);
             grpBxTransactions.FlatStyle = FlatStyle.Popup;
             grpBxTransactions.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            grpBxTransactions.Location = new Point(29, 206);
+            grpBxTransactions.Location = new Point(29, 267);
             grpBxTransactions.Margin = new Padding(4, 3, 4, 3);
             grpBxTransactions.Name = "grpBxTransactions";
             grpBxTransactions.Padding = new Padding(4, 3, 4, 3);
-            grpBxTransactions.Size = new Size(2150, 870);
+            grpBxTransactions.Size = new Size(2150, 809);
             grpBxTransactions.TabIndex = 7;
             grpBxTransactions.TabStop = false;
             grpBxTransactions.Text = "Recent Transaction";
@@ -113,24 +119,59 @@
             dgvRecentTransactions.Name = "dgvRecentTransactions";
             dgvRecentTransactions.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
             dgvRecentTransactions.RowHeadersWidth = 62;
-            dgvRecentTransactions.Size = new Size(2142, 819);
+            dgvRecentTransactions.Size = new Size(2142, 758);
             dgvRecentTransactions.TabIndex = 0;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(33, 25);
+            label2.Location = new Point(29, 81);
             label2.Name = "label2";
             label2.Size = new Size(308, 67);
             label2.TabIndex = 8;
             label2.Text = "Home Form";
+            // 
+            // toolStrip2
+            // 
+            toolStrip2.ImageScalingSize = new Size(40, 40);
+            toolStrip2.Items.AddRange(new ToolStripItem[] { toolStripDropdown_Graphs });
+            toolStrip2.Location = new Point(0, 0);
+            toolStrip2.Name = "toolStrip2";
+            toolStrip2.Size = new Size(2208, 52);
+            toolStrip2.TabIndex = 12;
+            toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripDropdown_Graphs
+            // 
+            toolStripDropdown_Graphs.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem_graph1, ToolStripMenuItem_Graph2 });
+            toolStripDropdown_Graphs.Image = (Image)resources.GetObject("toolStripDropdown_Graphs.Image");
+            toolStripDropdown_Graphs.ImageTransparentColor = Color.Magenta;
+            toolStripDropdown_Graphs.Name = "toolStripDropdown_Graphs";
+            toolStripDropdown_Graphs.Size = new Size(178, 45);
+            toolStripDropdown_Graphs.Text = "Graphs";
+            // 
+            // toolStripMenuItem_graph1
+            // 
+            toolStripMenuItem_graph1.Name = "toolStripMenuItem_graph1";
+            toolStripMenuItem_graph1.RightToLeftAutoMirrorImage = true;
+            toolStripMenuItem_graph1.Size = new Size(448, 54);
+            toolStripMenuItem_graph1.Text = "Graph 1";
+            toolStripMenuItem_graph1.Click += toolStripMenuItem_graph1_Click;
+            // 
+            // ToolStripMenuItem_Graph2
+            // 
+            ToolStripMenuItem_Graph2.Name = "ToolStripMenuItem_Graph2";
+            ToolStripMenuItem_Graph2.Size = new Size(448, 54);
+            ToolStripMenuItem_Graph2.Text = "Graph 2";
+            ToolStripMenuItem_Graph2.Click += ToolStripMenuItem_Graph2_Click;
             // 
             // HomeScreen
             // 
             AutoScaleDimensions = new SizeF(20F, 45F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2208, 1221);
+            Controls.Add(toolStrip2);
             Controls.Add(label2);
             Controls.Add(grpBxTransactions);
             Controls.Add(txtCurrentBalance);
@@ -143,6 +184,8 @@
             Text = "Finance Tracker";
             grpBxTransactions.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dgvRecentTransactions).EndInit();
+            toolStrip2.ResumeLayout(false);
+            toolStrip2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -155,5 +198,9 @@
         private TextBox txtCurrentBalance;
         private GroupBox grpBxTransactions;
         private Label label2;
+        private ToolStrip toolStrip2;
+        private ToolStripDropDownButton toolStripDropdown_Graphs;
+        private ToolStripMenuItem toolStripMenuItem_graph1;
+        private ToolStripMenuItem ToolStripMenuItem_Graph2;
     }
 }
