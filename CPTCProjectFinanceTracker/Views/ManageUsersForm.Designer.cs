4f289f9264bb36102f38fee212ed9d24d30560e1
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-03-03 11:03:39 -0800

    Created ManageUsersForm to be used as the UI to handle user CRUD

---
+++ CPTCProjectFinanceTracker/Views/ManageUsersForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CPTCProjectFinanceTracker.Views
+{
+    partial class ManageUsersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtbxUserName = new TextBox();
+            txtbxUserEmail = new TextBox();
+            txtbxUserPassword = new TextBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(75, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Name: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(80, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Email: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 300);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Password: ";
+            // 
+            // txtbxUserName
+            // 
+            txtbxUserName.Location = new Point(186, 98);
+            txtbxUserName.Name = "txtbxUserName";
+            txtbxUserName.Size = new Size(247, 31);
+            txtbxUserName.TabIndex = 3;
+            // 
+            // txtbxUserEmail
+            // 
+            txtbxUserEmail.Location = new Point(189, 192);
+            txtbxUserEmail.Name = "txtbxUserEmail";
+            txtbxUserEmail.Size = new Size(244, 31);
+            txtbxUserEmail.TabIndex = 4;
+            // 
+            // txtbxUserPassword
+            // 
+            txtbxUserPassword.Location = new Point(191, 297);
+            txtbxUserPassword.Name = "txtbxUserPassword";
+            txtbxUserPassword.Size = new Size(242, 31);
+            txtbxUserPassword.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(573, 84);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(148, 63);
+            btnAdd.TabIndex = 6;
+            btnAdd.Text = "ADD";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(573, 281);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(148, 63);
+            btnUpdate.TabIndex = 7;
+            btnUpdate.Text = "UPDATE";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // ManageUsersForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(txtbxUserPassword);
+            Controls.Add(txtbxUserEmail);
+            Controls.Add(txtbxUserName);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ManageUsersForm";
+            Text = "ManageUsersForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtbxUserName;
+        private TextBox txtbxUserEmail;
+        private TextBox txtbxUserPassword;
+        private Button btnAdd;
+        private Button btnUpdate;
+    }
+}
---
+++ CPTCProjectFinanceTracker/Views/ManageUsersForm.cs
@@ -0,0 +1,53 @@
+﻿using CPTCProjectFinanceTracker.Controllers;
+using CPTCProjectFinanceTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPTCProjectFinanceTracker.Views;
+public partial class ManageUsersForm : Form
+{
+    private readonly UserController _controller;
+    private readonly User _user;
+    public ManageUsersForm()
+    {
+        InitializeComponent();
+        _controller = new UserController();
+    }
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+        // Create a new user
+        User newUser = new User
+        {
+            UserName = txtbxUserName.Text,
+            UserEmail = txtbxUserEmail.Text,
+            UserPassword = txtbxUserPassword.Text,
+            CreatedAt = DateTime.Now,
+            Accounts = new List<Account>(),
+            Budgets = new List<Budget>(),
+            Reminders = new List<Reminder>()
+        };
+        // Create default account
+        Account defaultAccount = new Account
+        {
+            AccountName = "Default Account",
+            AccountBalance = 0,
+            User = newUser,
+            Transactions = new List<Transaction>()
+        };
+
+        // Add the default account to user's accounts collection
+        newUser.Accounts.Add(defaultAccount);
+        // Add the user to the database
+        _controller.AddUser(newUser);
+        Close();
+    }
+
+
+}
